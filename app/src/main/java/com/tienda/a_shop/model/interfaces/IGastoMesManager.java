b92fa60a95ad9cb2c9a4a86448d05ce18ac9f5af
leeanach <lorena_2328@hotmail.com>
2017-05-06 14:49:36 -0500

    Consulta de gasto mes actual y agregar categoria en tx

--- app/src/main/java/com/tienda/a_shop/model/interfaces/IGastoMesManager.java
+++ app/src/main/java/com/tienda/a_shop/model/interfaces/IGastoMesManager.java
@@ -1,6 +1,7 @@
 package com.tienda.a_shop.model.interfaces;
 
 import com.tienda.a_shop.entities.GastoMes;
+import com.tienda.a_shop.exceptions.InternalException;
 
 /**
  * Created by Lore on 06/05/2017.
@@ -10,5 +11,5 @@ import com.tienda.a_shop.entities.GastoMes;
 public interface IGastoMesManager {
 
     void agregarGastoMes(GastoMes gastoMes);
-    GastoMes obtenerGastoMesActual();
+    GastoMes obtenerGastoMesActual() throws InternalException;
 }
